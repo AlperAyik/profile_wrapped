a43656cbb4105f140613b241fbb22ec0d7b2cf64
Alper Ayik <Alper.ayik@student.rocva.nl>
2026-09-10 23:47:45 +0200

    sections skills en gaming toegevoegd en styling gedaan

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
 const h2Nav =  document.querySelectorAll('.navbar_logo');
+const cards = document.querySelectorAll('.cards');
+const skillsImg = document.querySelector('.skills_img');
 let interval = '';
 let timer = 0;
 let lastIndex = 0;
+let img_timer = 0;
+
+cards.forEach((card, index) => {
+    let slideUp = {
+        distance: '400%',
+        origin: 'left',
+        opacity: null,
+        delay: 500 * (index + 1),
+        reset: true,
+    };
+
+    ScrollReveal().reveal(card, slideUp);
+})
+
+skillsImg.addEventListener('mouseover', () => {
+    interval = setInterval(() => {
+        if(img_timer < 2) {
+            img_timer++;
+        } else {
+            clearInterval(interval);
+            img_timer = 0;
+            skillsImg.style.transform = 'rotate(0deg)';
+        }
+    }, 1000)
+})
+
+skillsImg.addEventListener('mouseout', () => {
+    clearInterval(interval);
+})
+
+
+
 
 setInterval(() => {
     if(timer < 4) {
